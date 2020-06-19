ba53e38b4d93be53f8791fad0f7d22b82de07963
Saleh-1998 <65109990+Saleh-1998@users.noreply.github.com>
2020-06-20 03:24:03 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Dinner from './Dinner.js';
+function App() {
+  return (
+    <div className="App">
+    
+    <Dinner dishName = "nimkeen ghost" />
+    </div>
+  );
+}
+export default App;
